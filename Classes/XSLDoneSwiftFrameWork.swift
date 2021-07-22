32327c76a69720b00bfa5d4bf0775be1feb2aee8
xiaosailing <xiaosailing@nodetower.com>
2021-07-22 17:19:36 +0800

    initial commit

---
+++ Classes/XSLDoneSwiftFrameWork.swift
@@ -0,0 +1,23 @@
+//
+//  XSLDoneSwiftFrameWork.swift
+//  XSLDoneSwiftFrameWork
+//
+//  Created by Apple06 on 2021/7/22.
+//
+
+import Foundation
+public class XSL : NSObject{
+   public func sayHello()  {
+        print("Say Hello to EveryOne" )
+        
+    }
+    
+    func saySomethingByUser(talk:String) {
+
+            print("Say Hello to EveryOne" )
+    }
+    
+    func getPassword() -> String {
+        return "123456"
+    }
+}
---
+++ Example/Pods/Target Support Files/XSLDoneSwiftFrameWork/XSLDoneSwiftFrameWork-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XSLDoneSwiftFrameWork : NSObject
+@end
+@implementation PodsDummy_XSLDoneSwiftFrameWork
+@end
